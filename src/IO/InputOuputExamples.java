d31e566bd40da448c5e01c5ef43d5afdec51ae08
Gaurav Pandey <gauravpandey44@gmail.com>
2015-08-24 12:22:33 +0530

    updated

---
+++ src/IO/InputOuputExamples.java
@@ -0,0 +1,15 @@
+package IO;
+
+import java.io.File;
+
+public class InputOuputExamples {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			File f=new File("abc.txt");
+			System.out.println(f.exists());
+		
+	}
+
+}
