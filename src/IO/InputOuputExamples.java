9dbb63af23d17ace5265c93807359f97cb39079b
Gaurav Pandey <gauravpandey44@gmail.com>
2015-08-24 15:21:13 +0530

    updated

--- src/IO/InputOuputExamples.java
+++ src/IO/InputOuputExamples.java
@@ -7,16 +7,24 @@ public class InputOuputExamples {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		try {
 			File f=new File("abc.txt");
 			System.out.println(f.exists());
-			try {
+			
 				f.createNewFile();
+				String sep=File.separator;
+			File f2=new File(sep+"Users/gp1384.ATTAPA.DOM/Documents/Eclipse/java_practice/abc2.txt");
+			//f2.createNewFile();
+			
+			System.out.println(f2.exists());
+			System.out.println(File.separator);
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
+				
 			}
-			System.out.println(f.exists());
+			
+			
 		
 	}
 
