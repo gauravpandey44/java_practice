982dc38b9cf3076aaef81a1a4e458ed88a529fd7
Gaurav Pandey <gauravpandey44@gmail.com>
2015-08-24 14:32:27 +0530

    update

--- src/IO/InputOuputExamples.java
+++ src/IO/InputOuputExamples.java
@@ -1,6 +1,7 @@
 package IO;
 
 import java.io.File;
+import java.io.IOException;
 
 public class InputOuputExamples {
 
@@ -8,6 +9,13 @@ public class InputOuputExamples {
 		// TODO Auto-generated method stub
 
 			File f=new File("abc.txt");
+			System.out.println(f.exists());
+			try {
+				f.createNewFile();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			System.out.println(f.exists());
 		
 	}
