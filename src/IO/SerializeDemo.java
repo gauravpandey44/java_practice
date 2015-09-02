f33680bbe37b46191da77f46d4d0163026a7d296
Gaurav Pandey <gauravpandey44@gmail.com>
2015-09-02 23:03:49 +0530

    Signed-off-by: Gaurav Pandey <gauravpandey44@gmail.com>

--- src/IO/SerializeDemo.java
+++ src/IO/SerializeDemo.java
@@ -3,8 +3,12 @@ package IO;
 import java.io.*;
 class Dog implements Serializable {
 	
-	int i=1,j=2;
-   transient final int c=3;
+	
+	int i=10,j=20;
+    static int c=300;
+   
+   
+    
 	
 }
 
@@ -18,11 +22,13 @@ public class SerializeDemo {
 		System.out.println("i= "+d1.i+"\nj= "+d1.j+"\nj= "+d1.c);
 		
 		//ObjectOutputStream oos=new ObjectOutputStream(new FileOutputStream("ObjectfileDog.txt"));
+		
 		//oos.writeObject(d1);
-		//oos.close();
+	//oos.close();
 		
 		ObjectInputStream ois=new ObjectInputStream(new FileInputStream("ObjectfileDog.txt"));
 		Dog d2=(Dog) ois.readObject();
+		
 		
 		System.out.println("After Deserialization");
 		System.out.println("i= "+d2.i+"\nj= "+d2.j+"\nj= "+d2.c);
